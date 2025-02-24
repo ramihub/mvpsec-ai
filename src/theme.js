1a6922e7c8c580d62ec56d09114364caf40f597e
mpetreaxyz <marius.petrea@happening.xyz>
2025-02-24 13:44:18 +0200

    recreate frontend

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    darkBlue: "#0A192F",
+    lightBlue: "#112240",
+    gray: "#8892B0",
+    teal: "#4FD1C5",
+    white: "#FFFFFF",
+    highlight: "#FFC107",
+  },
+  fonts: {
+    main: "'Roboto', sans-serif", // General UI, body text
+    heading: "'Montserrat', sans-serif", // Headlines, banners
+    code: "'IBM Plex Mono', monospace", // Code snippets, data displays
+    secondary: "'IBM Plex Sans', sans-serif", // Secondary text, maybe captions or less emphasized text
+    accent: "'Source Sans Pro', sans-serif", // UI elements, buttons, or small text that needs to be legible
+    highlight: "'Oswald', sans-serif" // Used for call-to-actions, important navigational links
+  },
+};
